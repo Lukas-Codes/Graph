ee29c7d93303dd5d051b22ced4246e38888ba586
Lukas.P <lukas.pascoe@gmail.com>
2020-07-03 22:33:02 -0400

    Update script.js
    Adding the scroll function

--- script.js
+++ script.js
@@ -218,8 +218,65 @@ function init() {
   graphRelease();
 }
 
-/* var isDevice = (/android|webos|iphone|ipad|ipod|blackberry/i.test(navigator.userAgent.toLowerCase()));
-if (isDevice) {
-  //select('#uiGroup').setAttribute('filter', '')
-} */
-/* TweenMax.globalTimeScale(0.5) */
+var fakeScroll = document.createElement('div')
+fakeScroll.className = 'fake-scroll'
+document.body.appendChild(fakeScroll)
+// In the `setupAnimation` function (below) we will set the `height` properly
+
+// Geeting dimmensions and setting up all for animation
+function setupAnimation () {
+  // Updating dimmensions
+  windowWidth = window.innerWidth
+  containerHeight = container.getBoundingClientRect().height
+  imageHeight = containerHeight / (windowWidth > 760 ? images.length / 2 : images.length)
+  // Set `height` for the fake scroll element
+  fakeScroll.style.height = containerHeight + 'px'
+  // Start the animation, if it is not running already
+  startAnimation()
+}
+
+// Update scroll `target`, and start the animation if it is not running already
+function updateScroll () {
+  target = window.scrollY || window.pageYOffset
+  startAnimation()
+}
+
+// Listen for `scroll` event to update `target` scroll position
+window.addEventListener('scroll', updateScroll)
+
+// Start the animation, if it is not running already
+function startAnimation () {
+  if (!rafActive) {
+    rafActive = true
+    rafId = requestAnimationFrame(updateAnimation)
+  }
+}
+
+// Do calculations and apply CSS `transform`s accordingly
+function updateAnimation () {
+  // Difference between `target` and `current` scroll position
+  var diff = target - current
+  // `delta` is the value for adding to the `current` scroll position
+  // If `diff < 0.1`, make `delta = 0`, so the animation would not be endless
+  var delta = Math.abs(diff) < 0.1 ? 0 : diff * ease
+
+  if (delta) { // If `delta !== 0`
+    // Update `current` scroll position
+    current += delta
+    // Round value for better performance
+    current = parseFloat(current.toFixed(2))
+    // Call `update` again, using `requestAnimationFrame`
+    rafId = requestAnimationFrame(updateAnimation)
+  } else { // If `delta === 0`
+    // Update `current`, and finish the animation loop
+    current = target
+    rafActive = false
+    cancelAnimationFrame(rafId)
+  }
+
+  // Update images (explained below)
+  updateAnimationImages()
+
+  // Set the CSS `transform` corresponding to the custom scroll effect
+  setTransform(container, 'translateY('+ -current +'px)')
+}
